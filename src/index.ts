f684b892fdde3ae75692bd093a169bd7994cc7de
egoavara <egoavara@gamil.com>
2022-07-22 16:29:47 +0900

    eslint 설정

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-import currency, {Options} from 'currency.js'
-import { pito, plugin } from 'pito'
+import currency ,{ Options , } from 'currency.js'
+import { pito ,plugin , } from 'pito'
 
-// moment, datetime
-// moment, date
-export type CurrencyExtra = {
-    $option? : Options,
-}
-export type CurrencyOption = {}
+/*
+ * Pito : moment, datetime
+ * moment, date
+ */
+export type CurrencyExtra = {$option? : Options ,}
+export type CurrencyOption = Record<string ,unknown>
 export type CurrencySchema = {type: 'string'}
-export type PitoCurrency = pito<string, currency, CurrencySchema, CurrencyOption, CurrencyExtra>
-export const PitoCurrency = (option?: Options): PitoCurrency => {
+export type PitoCurrency = pito<string ,currency ,CurrencySchema ,CurrencyOption ,CurrencyExtra>
+export const PitoCurrency = (option?: Options ,): PitoCurrency => {
     return {
-        type: 'string',
-        $option:option, 
-        $wrap(data) {
-            return data.format(this.$option)
-        },
-        $unwrap(raw) { return currency(raw, this.$option) },
-        $strict() {return {type: 'string'}},
-        $bypass() { return false },
-        $typeof : "class",
-        $constructor : currency,
+        type   : 'string' ,
+        $option: option , 
+        $wrap(data ,) {
+            return data.format(this.$option ,)
+        } ,
+        $unwrap(raw ,) { return currency(raw ,this.$option ,) } ,
+        $strict() { return {type: 'string' ,} } ,
+        $bypass() { return false } ,
+        $typeof     : 'class' ,
+        $constructor: currency ,
     }
 }
-// moment, time
-Object.defineProperty(plugin, 'Currency', { value: PitoCurrency, configurable: false, writable: false })
+// Moment, time
+Object.defineProperty(plugin ,'Currency' ,{ value: PitoCurrency ,configurable: false ,writable: false , } ,) 
 declare module 'pito' {
     interface PitoPlugin {
         Currency: typeof PitoCurrency
