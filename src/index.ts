89a40b91bf60737bdd18f4117b479b31b7757998
egoavara <egoavara@gamil.com>
2022-07-22 15:28:05 +0900

    기본적인 구현 완료

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-export { }
+import currency, {Options} from 'currency.js'
+import { pito, plugin } from 'pito'
+
+// moment, datetime
+// moment, date
+export type CurrencyExtra = {
+    $option? : Options,
+}
+export type CurrencyOption = {}
+export type CurrencySchema = {type: 'string'}
+export type PitoCurrency = pito<string, currency, CurrencySchema, CurrencyOption, CurrencyExtra>
+export const PitoCurrency = (option?: Options): PitoCurrency => {
+    return {
+        type: 'string',
+        $option:option, 
+        $wrap(data) {
+            return data.format(this.$option)
+        },
+        $unwrap(raw) { return currency(raw, this.$option) },
+        $strict() {return {type: 'string'}},
+        $bypass() { return false },
+        $typeof : "class",
+        $constructor : currency,
+    }
+}
+// moment, time
+Object.defineProperty(plugin, 'Currency', { value: PitoCurrency, configurable: false, writable: false })
+declare module 'pito' {
+    interface PitoPlugin {
+        Currency: typeof PitoCurrency
+    }
+    namespace pito {
+        type Currency = PitoCurrency
+    }
+}
